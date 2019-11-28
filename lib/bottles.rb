5a129830bb58a64126b7488fc7cde2c8aac8dbc1
Rodolfo V. Carmona Noguera <vladcarmona6@gmail.com>
2019-11-28 11:21:19 -0600

    merging the two verses more alike creating "container" function

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,24 +11,35 @@ class Bottles
     case number
     when 0
       "No more bottles of beer on the wall, " +
-          "no more bottles of beer.\n" +
-          "Go to the store and buy some more, " +
-          "99 bottles of beer on the wall.\n"
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, " +
-          "1 bottle of beer.\n" +
-          "Take it down and pass it around, " +
-          "no more bottles of beer on the wall.\n"
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, " +
-          "2 bottles of beer.\n" +
-          "Take one down and pass it around, " +
-          "1 bottle of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} #{container(number-1)} of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
-          "#{number} bottles of beer.\n" +
-          "Take one down and pass it around, " +
-          "#{number - 1} bottles of beer on the wall.\n"
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} #{container(number-1)} of beer on the wall.\n"
+    end
+  end
+
+  def container(number)
+    case number
+    when 1
+      "bottle"
+    when 6
+      "six-pack"
+    else
+      "bottles"
     end
   end
 end
